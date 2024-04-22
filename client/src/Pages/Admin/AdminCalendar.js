4738706bc1c5dcf722f4c74d387c1028df4e449d
Nimasha Heelibathdeniya <110526792+heelibathdeniyahanb@users.noreply.github.com>
2024-04-22 18:25:36 +0530

    Add header

--- client/src/Pages/Admin/AdminCalendar.js
+++ client/src/Pages/Admin/AdminCalendar.js
@@ -3,13 +3,16 @@ import React from 'react';
 import MiniCalendarUi from '../../Components/Calendar/MiniCalendarUi';
 import BigCalendarUi from '../../Components/Calendar/BigCalendarUi';
 import CalendarLogin from '../../Components/CalendarLogin';
+import Header from '../../Components/Header/Header';
 
 const AdminCalendar = () => {
     return (
         <div className='width-full'>
+             <Header/>
           <CalendarLogin/>
           <MiniCalendarUi/>
           <BigCalendarUi/>
+       
           
     
           
