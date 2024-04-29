c5ea793531e0c8ec2c9102594baa14395f59217f
Nimasha Heelibathdeniya <110526792+heelibathdeniyahanb@users.noreply.github.com>
2024-04-29 12:38:48 +0530

    update

--- Server/SolexCode.CRM.API.New/SolexCode.CRM.API.New/obj/Debug/net8.0/SolexCode.CRM.API.New.AssemblyInfo.cs
+++ Server/SolexCode.CRM.API.New/SolexCode.CRM.API.New/obj/Debug/net8.0/SolexCode.CRM.API.New.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolexCode.CRM.API.New")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7ca5874cf89153f39c14658ebe044dd1c891bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922cccb4ca0dc3856148b121fd3bc78e8c5c16d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolexCode.CRM.API.New")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolexCode.CRM.API.New")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
