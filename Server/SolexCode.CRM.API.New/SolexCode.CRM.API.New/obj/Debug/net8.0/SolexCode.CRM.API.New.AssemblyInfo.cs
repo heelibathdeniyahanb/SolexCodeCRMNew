bf35eb3c4213b964e97dca49469194a026eae313
Naveesha1 <110526623+Naveesha1@users.noreply.github.com>
2024-05-12 16:00:04 +0530

    Create and Update part fix
    task card can be created and updated.

--- Server/SolexCode.CRM.API.New/SolexCode.CRM.API.New/obj/Debug/net8.0/SolexCode.CRM.API.New.AssemblyInfo.cs
+++ Server/SolexCode.CRM.API.New/SolexCode.CRM.API.New/obj/Debug/net8.0/SolexCode.CRM.API.New.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolexCode.CRM.API.New")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b20201b47c1a243fc760d31c32d27896ae978d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a6251d3da96518ddab811070b853faf754e204")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolexCode.CRM.API.New")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolexCode.CRM.API.New")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
