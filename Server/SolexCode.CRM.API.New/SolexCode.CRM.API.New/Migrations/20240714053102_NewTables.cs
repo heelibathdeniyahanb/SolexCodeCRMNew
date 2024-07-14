81b3248ce69410fd956da1ded2d943fac3b1b6ad
Nimasha Heelibathdeniya <110526792+heelibathdeniyahanb@users.noreply.github.com>
2024-07-14 12:19:08 +0530

    Do some modifications
    Migrations, database update, modify nav bars, install stripe

---
+++ Server/SolexCode.CRM.API.New/SolexCode.CRM.API.New/Migrations/20240714053102_NewTables.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolexCode.CRM.API.New.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NewLeadId",
+                table: "Invoice",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NewLeadId1",
+                table: "Invoice",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Invoice",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<long>(type: "bigint", nullable: false),
+                    PaymentMethodId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentIntentId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientCompany = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoice_NewLeadId",
+                table: "Invoice",
+                column: "NewLeadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoice_NewLeadId1",
+                table: "Invoice",
+                column: "NewLeadId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoice_UserId",
+                table: "Invoice",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_UserId",
+                table: "Payments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoice_NewLeads_NewLeadId",
+                table: "Invoice",
+                column: "NewLeadId",
+                principalTable: "NewLeads",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoice_NewLeads_NewLeadId1",
+                table: "Invoice",
+                column: "NewLeadId1",
+                principalTable: "NewLeads",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoice_Users_UserId",
+                table: "Invoice",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoice_NewLeads_NewLeadId",
+                table: "Invoice");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoice_NewLeads_NewLeadId1",
+                table: "Invoice");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoice_Users_UserId",
+                table: "Invoice");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoice_NewLeadId",
+                table: "Invoice");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoice_NewLeadId1",
+                table: "Invoice");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoice_UserId",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "NewLeadId",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "NewLeadId1",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Invoice");
+        }
+    }
+}
--- client/src/Pages/Client/ClientDashboard.js
+++ client/src/Pages/Client/ClientDashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ClientSideNavBar from '../../Components/Client/ClientSideNavBar'
 
 import Card1 from '../../Components/Client/ClientDashboardComponents/Card1'
@@ -7,8 +7,28 @@ import Card3 from '../../Components/Client/ClientDashboardComponents/Card3'
 import ClientDashboardOngoingLeads from '../../Components/Client/ClientDashboardComponents/ClientDashboardOngoingLeads'
 import ClientScheduledEventsTasks from '../../Components/Client/ClientDashboardComponents/ClientSheduledEventsTasks'
 import ClientHeader from '../../Components/Header/ClientHeader'
+import Chat from '../../Components/Chat/Chat'
+import { useUser } from '../../Components/login/UserContext'
+import { IoIosChatboxes } from "react-icons/io";
+import { chatConnection, notificationConnection } from '../../Components/Chat/Connection';
 
 export default function ClientDashboard() {
+
+  const [showChat, setShowChat] = useState(false); 
+  const [unreadMessages, setUnreadMessages] = useState(0);
+  const { userData } = useUser();
+
+  // Function to toggle chat popup
+  const toggleChat = () => {
+    setShowChat(!showChat);
+    if (!showChat) {
+      setUnreadMessages(0); // Reset unread messages count when chat is opened
+    }
+  };
+
+  const closeChat = () => {
+    setShowChat(false);
+  };
   return (
     <div>
         <ClientHeader/>
@@ -25,6 +45,23 @@ export default function ClientDashboard() {
             <ClientDashboardOngoingLeads />
             <ClientScheduledEventsTasks/>
           </div></div></div>
+
+          <div className="fixed bottom-0 right-0 m-4">
+        <IoIosChatboxes className="text-4xl text-blue-600 cursor-pointer" onClick={toggleChat} />
+        {unreadMessages > 0 && (
+          <span className="absolute top-0 right-0 bg-red-600 text-white rounded-full text-xs w-6 h-6 flex items-center justify-center">
+            {unreadMessages}
+          </span>
+        )}
+      </div>
+      {/* Chat popup */}
+      {showChat && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-4 rounded-lg">
+            <Chat onClose={closeChat} />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
